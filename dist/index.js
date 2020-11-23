540397bfe2dcded1e5f1f639d8c08cb8ed470e92
Shunsuke Ito <shunsuke.ito@gmail.com>
2020-11-23 14:10:18 +0900

    refactoring names

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getLocaleList = exports.axeLocale = void 0;
 const locales_json_1 = require("./locales.json");
 // comment
-function requireLocation(lang) {
+function requireLocale(lang) {
     if (locales_json_1.locales.includes(lang)) {
-        const AXE_LOCALE = require(`axe-core/locales/${lang}.json`);
-        return AXE_LOCALE;
+        try {
+            const AXE_LOCALE = require(`axe-core/locales/${lang}.json`);
+            return AXE_LOCALE;
+        }
+        catch (error) {
+            return false;
+        }
     }
     else {
         return false;
@@ -15,10 +20,10 @@ function requireLocation(lang) {
 function axeLocale(lang) {
     if (lang.length === 0 || lang === 'auto') {
         const language = (window.navigator.languages && window.navigator.languages[0]) || window.navigator.language;
-        return requireLocation(language);
+        return requireLocale(language);
     }
     else {
-        return requireLocation(lang);
+        return requireLocale(lang);
     }
 }
 exports.axeLocale = axeLocale;
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,14 @@ interface localeArray extends Array<locale> {}
 import {locales} from './locales.json';
 
 // comment
-function requireLocation(lang: string): any {
+function requireLocale(lang: string): any {
   if(locales.includes(lang)){
-    const AXE_LOCALE = require(`axe-core/locales/${lang}.json`);
-    return AXE_LOCALE;
+    try {
+      const AXE_LOCALE = require(`axe-core/locales/${lang}.json`);
+      return AXE_LOCALE;
+    } catch ( error ){
+      return false;
+    }
   }
   else{
     return false;
@@ -19,10 +23,10 @@ function requireLocation(lang: string): any {
 export function axeLocale (lang: string) {
   if( lang.length === 0 || lang === 'auto') {
     const language = (window.navigator.languages && window.navigator.languages[0]) || window.navigator.language;
-    return requireLocation(language);
+    return requireLocale(language);
   }
   else{
-    return requireLocation(lang);
+    return requireLocale(lang);
   }
 }
 
